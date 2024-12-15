6c0ecf77ed0fd554662568d92c0a646387a72fac
Rakesh Jain <rakeshjain@Rakeshs-MacBook-Pro.local>
2024-12-15 15:09:13 -0500

    Initial commit of Spring Boot application

---
+++ src/test/java/com/projects/retail_backend/RetailBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.retail_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
